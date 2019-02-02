e747f063ccabe41cf161032ec1f9fb8ac2f721a1
Oleksii Maiboroda <alexeymayboroda23@gmail.com>
2019-02-02 13:30:11 +0200

    some minor change

--- index.php
+++ index.php
@@ -2,48 +2,35 @@
 $nominalArray = [1, 2, 5, 10, 20, 50, 100, 200, 500];
 $summa = $argv[1];
 $result = [];
-
-
 checkMaxLimit($summa);
 nominalNumCount($summa, $nominalArray, $result);
 printResult($result);
-
-
 function checkMaxLimit($summa)
 {
     $maxLimit = 100000;
-
     if ($summa > $maxLimit) {
         exit('Max summ 100k' . \PHP_EOL);
     }
 }
-
 function nominalNumCount($summa, $nominalArray, &$result)
 {
     $nominal = \array_pop($nominalArray);
-
     if ($summa <= $nominal) {
         $nominal = \array_pop($nominalArray);
     }
-
     if ($summa % $nominal) {
-        [$total, $rest] = \explode('.', $summa / $nominal);
+        list($total, $rest) = \explode('.', $summa / $nominal);
     } else {
         $total = $summa / $nominal;
     }
-
     if ($total > 0) {
         $result[$nominal] = $total;
     }
-
-
     if (isset($rest)) {
         $rest = $summa - $total * $nominal;
         nominalNumCount($rest, $nominalArray, $result);
     }
 }
-
-
 function printResult($result)
 {
     foreach ($result as $key => $val) {
